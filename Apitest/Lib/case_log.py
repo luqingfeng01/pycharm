cfbc25b994b3ee74462790340dde5ae11312f3fc
luqingfeng01 <274832258@qq.com>
2019-08-23 17:42:52 +0800

    练习

---
+++ Apitest/Lib/case_log.py
@@ -0,0 +1,54 @@
+from pycharm.Apitest.Config.config import *
+import pymysql
+import sys
+sys.path.append('..') # 提升一级到项目更目录下
+from pycharm.Apitest.Config.config import  * # 从项目根目录下导入
+
+#获取连接方法
+def get_db_conn():
+    conn = pymysql.connect(host=host,
+                           port=port,
+                           user=user,
+                           password=password,
+                           db=database,
+                           charest="utf8") # 如果查询有中文，需要指定测试集编码
+    return conn
+
+
+# 封装数据库查询操作
+def query_db(sql):
+    conn = get_db_conn()  #建立连接
+    cur = conn.cursor()   #建立游标
+    cur.execute(sql)                      #查询语句
+    result = cur.fetchall()  #获取所有结果
+    cur.close()    #关闭游标
+    conn.close()     #关闭连接
+    return result
+
+
+# 封装更改数据库操作
+def change_db(sql):
+    conn = get_db_conn()  # 建立连接
+    cur = conn.cursor()  # 建立游标
+    try:
+        cur.execute(sql)
+        conn.commit()  #直接语句
+    except Exception as  e:
+        conn.rollback()  #回滚
+    finally:
+        cur.close()
+        conn.close()
+
+# 封装常用数据库操作
+# 封装常用数据库操作
+def check_user(name):
+ # 注意sql中''号嵌套的问题
+    sql = "select * from user where name = '{}'".format(name)
+    result = query_db(sql)
+    return True if result else False
+def add_user(name, password):
+    sql = "insert into user (name, passwd) values ('{}','{}')".format(name, password)
+    change_db(sql)
+def del_user(name):
+    sql = "delete from user where name='{}'".format(name)
+    change_db(sql)
