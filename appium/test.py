b4a0d57357fafe5930beeb37d45be0031973938b
luqingfeng01 <274832258@qq.com>
2019-08-23 17:48:09 +0800

    提交

---
+++ appium/test.py
@@ -0,0 +1,10 @@
+from time import sleep
+from appium import webdriver
+desired_caps = {'platformName':'Android',
+                'deviceName':'127.0.0.1:62001',
+                'platformVersion':'3.8.3.1',
+                'appPackage':'com.juyang.mall',
+                'appActivity':'com.shanjian.juyang.activity.home.Activity_Home'
+                }
+driver=webdriver.Remote('http://127.0.0.1:4723/wd/hub',desired_caps)
+sleep(4)
