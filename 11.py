65d09415f09baa08453fcb379f7e3b038582c108
luqingfeng01 <274832258@qq.com>
2019-09-15 23:14:16 +0800

    提交

--- 11.py
+++ 11.py
@@ -1,3 +1,117 @@
-import os
+'''编写一个通讯录程序，实现增删改查功能
+#### 1.设计数据结构
+一条记录： 姓名，电话， id
+一个通讯录：列表，里面元素为记录
 
-os.system("locust.exe -f E:\pycharm\test3.py")
+#### 2. 函数设计
+* 增加 add_record
+* 查询 query_record
+* 修改 change_record
+* 删除 delete_record
+
+#### 3. 菜单设计
+* main函数
+* while 循环
+* 选择相应功能
+示例
+````
+
+        通讯录
+        1. 添加
+        2. 查找
+        3. 删除
+        4. 修改
+        5. 退出
+
+请选择操作:1
+请输入姓名:jia
+请输入电话:123
+添加成功
+
+        通讯录
+        1. 添加
+        2. 查找
+        3. 删除
+        4. 修改
+        5. 退出
+
+请选择操作:
+````
+'''
+record_dict = {}
+recordid = 0
+
+
+def add_record():
+    name = input("请输入要添加的联系人姓名")
+    if name in record_dict:
+        print("用户已经存在")
+    else:
+        phone = input("请输入联系人电话")
+        global recordid
+        recordid += 1
+
+        l = {"id": recordid, "电话": phone}
+        record_dict[name] = l
+        print("添加成功")
+
+
+def query_record():
+    name = input("请输入要查询的联系人姓名")
+    if name in record_dict:
+        print('id:%s\n联系人:%s \n电话号码是:%s ' %
+              (record_dict[name]["id"], name, record_dict[name]['电话']))
+    else:
+        print("输入的名字不存在")
+
+
+def change_record():
+    name = input("请输入要修改的联系人名字")
+    print(record_dict)
+    if name in record_dict:
+        phone = input("请输入要修改的电话")
+        record_dict[name]["id"] = recordid
+
+        record_dict[name]["电话"] = phone
+        print("修改成功")
+    else:
+
+        print("联系人%s不存在" % name)
+
+
+def delete_record():
+    name = input("请输入要删除的联系人名字")
+    if name in record_dict:
+        del record_dict[name]
+        print("删除成功")
+    else:
+        print('联系人 %s 不存在' % name)
+
+
+if __name__ == '__main__':
+    while True:
+
+        meau = '''
+            通讯录
+            1. 添加
+            2. 查找
+            3. 删除
+            4. 修改
+            5. 退出
+            '''
+        print(meau)
+        try:
+            s = input("请输入对应数字")
+            if s == "1":
+                add_record()
+
+            elif s == "2":
+                query_record()
+            elif s == "3":
+                delete_record()
+            elif s == "4":
+                change_record()
+            elif s == "5":
+                break
+        except Exception as f:
+            print("输入有误:%s" % f)
--- demo1.py
+++ demo1.py
@@ -1,24 +1,86 @@
-from flask import Flask,request,jsonify,render_template
-#render_template渲染模板
+import pickle as p
 
+class person:
+    notes={}
+    def add(self):
+        name = input('请输入要添加的联系人姓名')
+        if name in person.notes:
+            print('该联系人已经存在')
+        else:
+          telephone =  (input('请输入联系人电话号码'))
+          addr =  (input('请输入联系人地址'))
+          label={'电话':telephone,'地址':addr}
+          person.notes[name]=label
 
-app = Flask(__name__)
+    def dele(self):
+      name = (input('请输入要删除的联系人姓名'))
+      if name in person.notes:
+        del person.notes[name]
+        print ("%s" %  person.notes.items())
+      else:
+        print('联系人 %s 不存在'%name)
 
-data = [
-    {"case":"test_sub_01","result":"PASS"},
-    {"case":"test_sub_02","result":"FAIL"},
-    {"case":"test_sub_03","result":"PASS"},
-    {"case":"test_sub_04","result":"PASS"},
-]
+    def search(self):
+      name = (input('请输入要搜索的联系人姓名'))
+      if name in person.notes:
+         print('联系人 %s 的电话号码是 %s , 地址是 %s'%(name,person.notes[name]['电话'],person.notes[name]['地址']))
+      else:
+         print('联系人 %s 不存在'%name)
 
-@app.route("/login")
-def login():
-    return render_template("login.html",
-                           tester="tim",
-                           pass_num=3,
-                           fail_num=0,
-                           data=data
-                           )
+    def modify(self):
+      name = (input('请输入要编辑的联系人姓名'))
+      if name in person.notes:
+        telephone =  (input('请输入联系人电话号码'))
+        addr =  (input('请输入联系人地址'))
+        person.notes[name]['电话']=telephone
+        person.notes[name]['地址']=addr
+      else:
+         print('联系人 %s 不存在，若要编辑请选择添加选项'%name)
+    def write(self):
+      f = open('联系人.txt','wb+')
+      p.dump(person.notes,f)
+      f.close()
+    def read(self):
+      file = '联系人.txt'
+      try:
+        f = open(file ,'rb+')
+        person.notes = p.load(f)
+        f.close()
+      except:
+        f = open(file ,'w')
+        f.close()
+    def show(self):
+      print(person.notes)
 
-if __name__ == '__main__':
-    app.run()
+def menu():
+    print('''系统提供以下功能
+    1：添加
+    2：删除
+    3：修改
+    4：搜索
+    5：退出
+    6: 显示全部联系人信息''')
+
+people = person()
+people.read()
+while True:
+  try:
+    menu()
+    choice = int(input('请输入相应数字操作'))
+    if choice==1:
+        people.add()
+    elif choice ==2:
+        people.dele()
+    elif choice ==3:
+        people.modify()
+    elif choice ==4:
+        people.search()
+    elif choice ==5:
+        people.write()
+        break
+    elif choice==6:
+        people.show()
+    else:
+        print('输入不合法，请输入合法数字')
+  except ValueError:
+      print('请输入数字选项')
